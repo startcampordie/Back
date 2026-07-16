527ad11ca5a41af7342c509a71583dd35a3a6db0
KJW <comkjo0512@gmail.com>
2026-07-16 12:08:09 +0900

    fix: cors 해결

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://ronamdo.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
